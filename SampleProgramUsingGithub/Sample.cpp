2f3c69ea7d1c4db3285b038b6eb6b479ae391186
ayanclover <nayaclover11@gmail.com>
2024-10-04 23:07:39 +0800

    updated the main cpp
    using visual studio 2022 updated the main cpp

--- SampleProgramUsingGithub/Sample.cpp
+++ SampleProgramUsingGithub/Sample.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 	cout << "Print out your name in the code and commit so we can see the merge"  << endl ; 
 	cout << "Ian Clavero" << endl;
+	cout << "Updated using visual studio 2022" << endl;
 
 
 
