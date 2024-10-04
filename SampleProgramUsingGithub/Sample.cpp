1341d38d2750e017ca7039a90a072f1df7fddc12
ayanclover <nayaclover11@gmail.com>
2024-10-04 23:01:42 +0800

    Add project files.

---
+++ SampleProgramUsingGithub/Sample.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Print out your name in the code and commit so we can see the merge"  << endl ; 
+	cout << "Ian Clavero" << endl;
+
+
+
+	return 0;
+}
